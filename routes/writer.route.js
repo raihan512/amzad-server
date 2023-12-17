0cb04f265283926b2a8974724407e0e475d9077d
raihan512 <raihangazi1024@gmail.com>
2023-12-17 12:15:14 +0600

    get writers by post method

--- routes/writer.route.js
+++ routes/writer.route.js
@@ -6,7 +6,7 @@ router.get("/", async (req, res) => {
   try {
     Writer.find()
       .then((Writer) => {
-        res.status(200).json({ Writer: Writer });
+        res.status(200).json(Writer);
       })
       .catch((error) => {
         console.log(error);
@@ -18,13 +18,24 @@ router.get("/", async (req, res) => {
   }
 });
 // Get writer by search writer id
-router.get("/:customId", async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const result = await Writer.find({ wirterId: req.params.customId });
-    res.json(result);
+    const writer = await Writer.find({ wirterId: req.params.id });
+    res.status(200).json(writer);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/getwriters", async (req, res) => {
+  try {
+    const keys = req.body.flat();
+    const writers = await Writer.find({ writerId: { $in: keys } });
+    res.status(200).json(writers);
+  } catch (error) {
+    console.error("Error loading writers:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 module.exports = router;
