3ed4e0112efd133cb9b25a2c766b02555b17485f
raihan512 <raihangazi1024@gmail.com>
2023-12-18 05:03:27 -0600

    database connected and load all books

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
 const app = express();
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-const Book = require("./routes/book.route");
-const Publisher = require("./routes/publishers.route");
-const Writer = require("./routes/writer.route");
-const Editor = require("./routes/editor.route");
-const Category = require("./routes/category.route");
-const SubCategory = require("./routes/subcategory.route");
+app.get("/", (req, res) => {
+  res.send("Welcome to Maktabatul Amzad Server");
+});
 
-const connectDB = require("./utils/connectdb");
-app.use("/api/books", Book);
-app.use("/api/publisher", Publisher);
-app.use("/api/writer", Writer);
-app.use("/api/editor", Editor);
-app.use("/api/categories", Category);
-app.use("/api/subcategory", SubCategory);
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://amzad2023:Raihan1234@maktabatul-amzad.vgl3kbg.mongodb.net/?retryWrites=true&w=majority";
 
-connectDB();
-const port = 3000;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const database = client.db("maktabatul-amzad");
+    const books = database.collection("books");
+
+    // ----------------------------------------------------------Book Route----------------------------------------------------------
+    app.get("/api/book", async (req, res) => {
+      const allBooks = await books.find().toArray();
+      res.status(200).json(allBooks);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Database Connected");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log("Server is running");
+  console.log(`Server is running on port ${port}`);
 });
